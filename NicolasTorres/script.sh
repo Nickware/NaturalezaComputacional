ed823ea363d042b74e8bf9e675b6ce2fcf220ff5
Nicolas Torres <nickthor@dm3-1148la>
2017-06-02 05:58:20 -0500

    optimizando bloque de calculo: lineas de salida para archivo git

--- NicolasTorres/script.sh
+++ NicolasTorres/script.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 #Author		: N.Torres
-#Date		: 04/28/2017
-#Version	: 0.0.2 
-echo "Limpiando carpeta de archivos anteriores"
-rm *.gnu
+#Date		: 26/10/2017
+#Version	: 0.0.5 
+#echo "Limpiando carpeta de archivos anteriores"
+for i in *.gnu *.gif; 
+	do	rm $i
+			echo "Borrando archivos anteriores "
+	done
+#********************Bloque Uno: Inicializacion*********************#
 echo "Inicia..."
 echo "Programa grafica funciones matematicas"
 echo "-------------Warning------------------"
 echo "Este programa utiliza sintaxis gnuplot"
-rm *.gnu
+#rm [*.gnu, *.gif]
 read -p "Ingrese la primera funcion: " funcion01
 read -p "Ingrese la segunda funcion: " funcion02
-read -p "Nombre de la grafica (entre comillas): " titulo
-read -p "Titulo de la etiqueta de la ordenada (entre comillas): " ordenada
-read -p "Titulo de la etiqueta de la abscisa (entre comillas): " abscisa
+read -p "Nombre de la grafica : " titulo
+read -p "Titulo de la etiqueta de la ordenada : " ordenada
+read -p "Titulo de la etiqueta de la abscisa : " abscisa
 read -p "Tipo de imagen: " tipo
 #read -p "Nombre de la imagen a guardar (entre comillas): " imagen
-echo "plot" $funcion01 "w l," $funcion02 "w p" >> script.gnu
-echo 'set title' $titulo >> script.gnu
-echo 'set ylabel' $ordenada >> script.gnu
-echo 'set xlabel' $abscisa >> script.gnu
-echo 'set xrange[0:90]' >> script.gnu
-echo 'set terminal' $tipo >> script.gnu
-#echo 'set output' $imagen >> script.gnu
-echo 'set output' '"imagen01'.$tipo'"' >> script.gnu
-echo 'replot' >> script.gnu
-echo 'set xrange[91:180]' >> script.gnu
-echo 'set terminal' $tipo >> script.gnu
-#echo 'set output' $imagen >> script.gnu
-echo 'set output' '"imagen02'.$tipo'"' >> script.gnu
-echo 'replot' >> script.gnu
-echo 'set xrange[181:270]' >> script.gnu
-echo 'set terminal' $tipo >> script.gnu
-#echo 'set output' $imagen >> script.gnu
-echo 'set output' '"imagen03'.$tipo'"' >> script.gnu
-echo 'replot' >> script.gnu
-echo 'set xrange[271:360]' >> script.gnu
-echo 'set terminal' $tipo >> script.gnu
-#echo 'set output' $imagen >> script.gnu
-echo 'set output' '"imagen04'.$tipo'"' >> script.gnu
-echo 'replot' >> script.gnu
-gnuplot script.gnu
-#evince $imagen
-convert -delay 10 -loop 0 *.png animation.gif 
+#********************Bloque Dos: Calculo***************************#
+#echo "plot" $funcion01"(x)" "w l," $funcion02"(x)" "w p" >> script.gnu
+echo 'set title' '"'$titulo'"' >> script.gnu
+echo 'set ylabel' '"'$ordenada'"' >> script.gnu
+echo 'set xlabel' '"'$abscisa'"' >> script.gnu
+if [ $tipo = gif ]
+then
+#	
+read -p "Ingrese el numero de intervalos(frames): " ni
+	echo "ni="$ni >> script.gnu
+	#echo 'reset' >> script.gnu
+	echo 'set terminal gif animate' >> script.gnu
+	echo 'set output' '"imagen'.$tipo'"' >> script.gnu
+	echo 'dt=2*pi/ni' >> script.gnu
+	echo 'set xrange [0:4*dt]' >> script.gnu
+	echo 'i=0' >> script.gnu
+	echo 'load' '"animate.gnu"' >> script.gnu
+	echo 'set output' >> script.gnu
+	echo "plot" $funcion01'(x+i*dt) w l title sprintf(''"t=%i"'',i),'$funcion02' (x+i*dt) w p title sprintf(''"t=%i"'',i)' >> animate.gnu
+	echo 'i=i+1' >> animate.gnu
+	echo 'if (i < ni) reread' >> animate.gnu
+else
+#	
+	echo 'set terminal' $tipo >> script.gnu
+	echo 'set output' '"imagen'.$tipo'"' >> script.gnu
+fi
+##******************Bloque Tres: Salida  **************************#
